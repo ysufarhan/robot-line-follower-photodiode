e0f63fec13a176f021aea8288d5ae534f059e19b
Muhammad Yusuf Farhani <muhammadyusuf.2023@student.uny.ac.id>
2025-11-08 16:47:02 +0700

    Update

--- kontrol_motorDC.ino
+++ kontrol_motorDC.ino
@@ -1,81 +1,83 @@
-// === Definisi Pin Motor ===
-#define pin_pwm_motor_L 6     // ENA (L298N kiri)
-#define pin_dir_motor_L 9     // IN1
-#define pin_pwm_motor_R 5     // ENB (L298N kanan)
-#define pin_dir_motor_R 10    // IN3
+// === Definisi Pin Motor (L293D) ===
+// Motor kiri
+#define IN1 9   // arah 1
+#define IN2 6   // arah 2
+// Motor kanan
+#define IN3 5   // arah 1
+#define IN4 10  // arah 2
 
 // === Variabel arah motor ===
 boolean LEFT  = 0;
 boolean RIGHT = 0;
 
-// Fungsi untuk atur polaritas motor (kalau arah salah, ubah di sini)
+// Fungsi untuk mengatur polaritas motor (kalau arah terbalik, ubah di sini)
 void polaritasMotor(boolean left, boolean right)
 {
   LEFT  = left;
   RIGHT = right;
 }
 
-// Fungsi utama untuk mengatur kecepatan & arah motor
+// Fungsi untuk mengatur arah motor (tanpa PWM, karena ENA & ENB langsung ke +5V)
 void setMotor(int PWM_L, int PWM_R)
 {
-  // Set polaritas default (ubah sesuai arah putar motor)
+  // polaritas default
   polaritasMotor(0, 1);
 
   // --- MOTOR KANAN ---
   if (RIGHT == 0) {
     if (PWM_R >= 0) {
-      digitalWrite(pin_dir_motor_R, HIGH);   // arah maju
+      digitalWrite(IN3, HIGH);   // maju
+      digitalWrite(IN4, LOW);
     } else {
-      digitalWrite(pin_dir_motor_R, LOW);    // arah mundur
-      PWM_R = -PWM_R; // ubah jadi positif
+      digitalWrite(IN3, LOW);    // mundur
+      digitalWrite(IN4, HIGH);
     }
-    analogWrite(pin_pwm_motor_R, constrain(PWM_R, 0, 255));
   } 
-  else { // Jika polaritas dibalik
+  else { // polaritas dibalik
     if (PWM_R >= 0) {
-      digitalWrite(pin_dir_motor_R, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
     } else {
-      digitalWrite(pin_dir_motor_R, HIGH);
-      PWM_R = -PWM_R;
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
     }
-    analogWrite(pin_pwm_motor_R, constrain(PWM_R, 0, 255));
   }
 
   // --- MOTOR KIRI ---
   if (LEFT == 0) {
     if (PWM_L >= 0) {
-      digitalWrite(pin_dir_motor_L, HIGH);   // arah maju
+      digitalWrite(IN1, HIGH);   // maju
+      digitalWrite(IN2, LOW);
     } else {
-      digitalWrite(pin_dir_motor_L, LOW);    // arah mundur
-      PWM_L = -PWM_L;
+      digitalWrite(IN1, LOW);    // mundur
+      digitalWrite(IN2, HIGH);
     }
-    analogWrite(pin_pwm_motor_L, constrain(PWM_L, 0, 255));
   } 
-  else { // Jika polaritas dibalik
+  else { // polaritas dibalik
     if (PWM_L >= 0) {
-      digitalWrite(pin_dir_motor_L, LOW);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
     } else {
-      digitalWrite(pin_dir_motor_L, HIGH);
-      PWM_L = -PWM_L;
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
     }
-    analogWrite(pin_pwm_motor_L, constrain(PWM_L, 0, 255));
   }
 }
 
 void setup() 
 {
   // Inisialisasi pin
-  pinMode(pin_pwm_motor_L, OUTPUT);
-  pinMode(pin_dir_motor_L, OUTPUT);
-  pinMode(pin_pwm_motor_R, OUTPUT);
-  pinMode(pin_dir_motor_R, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 
-  // Tes motor
-  setMotor(100, 100);   // maju pelan
+  // Tes motor seperti program 1 versi L293D
+  setMotor(1, 1);     // maju
   delay(2000);
-  setMotor(150, -30);   // belok kanan
+  setMotor(1, -1);    // belok kanan
   delay(2000);
-  setMotor(0, 0);       // stop
+  setMotor(0, 0);     // stop
 }
 
 void loop() 
