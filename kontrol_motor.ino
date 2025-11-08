ef8c5f43a6809fac3d5321dcd0a1d1156c6146b2
Muhammad Yusuf Farhani <muhammadyusuf.2023@student.uny.ac.id>
2025-11-08 16:45:50 +0700

    Program maju terus

--- kontrol_motor.ino
+++ kontrol_motor.ino
@@ -1,78 +1,28 @@
-// Pin kontrol motor
-#define ENA 5    // Enable motor kiri (PWM)
-#define IN1 6    // Arah motor kiri
-#define IN2 7
-#define ENB 9    // Enable motor kanan (PWM)
-#define IN3 10   // Arah motor kanan
-#define IN4 11
+// Program: Dua Motor Maju (L293D + Arduino Nano)
+// Enable A dan B dihubungkan langsung ke +5V
+// Motor kiri: IN1 = D9, IN2 = D6
+// Motor kanan: IN3 = D5, IN4 = D10
 
-int speedMotor = 180;  // kecepatan (0–255)
+// Definisi pin motor
+const int IN1 = 9;   // Motor kiri
+const int IN2 = 6;
+const int IN3 = 5;   // Motor kanan
+const int IN4 = 10;
 
 void setup() {
-  pinMode(ENA, OUTPUT);
+  // Set pin sebagai output
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
-  pinMode(ENB, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  Serial.begin(9600);
-  Serial.println("Kontrol Motor Sederhana Siap!");
+  // Motor maju
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
 void loop() {
-  // Contoh urutan gerak otomatis
-  maju();
-  delay(2000);
-
-  kanan();
-  delay(1000);
-
-  kiri();
-  delay(1000);
-
-  berhenti();
-  delay(2000);
-}
-
-// ====== FUNGSI MOTOR ======
-
-void maju() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, speedMotor);
-  analogWrite(ENB, speedMotor);
-  Serial.println("F");
-}
-
-void kanan() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, speedMotor);
-  analogWrite(ENB, 0);
-  Serial.println("R");
-}
-
-void kiri() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, speedMotor);
-  Serial.println("L");
-}
-
-void berhenti() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
-  Serial.println("S");
+  // Tidak ada perulangan, motor terus maju
 }
